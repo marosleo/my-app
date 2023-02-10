7612ee532e60a2f6cdf0030f8b4e172bf8557d55
marosleo <marosleo1990@gmail.com>
2023-02-10 08:47:43 -0300

    Rotas-arrumadas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import {useState} from 'react'
-
-import "./App.css";
-import SeuNome from './components/SeuNome'
-import Saudacao from './components/Saudacao';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Contato from './pages/Contato'
+import Empresa from './pages/Empresa'
+import NavBar from './components/layouts/Navbar'
+import Footer from './components/layouts/Footer' 
 
 function App() {
-  const [nome, setNome] = useState()
 
   return (
-    <div className="App">
-     <h1>State Lift</h1>
-     <SeuNome setNome={setNome} />
-     <Saudacao nome={nome}/>
-    </div>
-  );
+    <Router>
+      <NavBar/>
+      <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/contato" element={<Contato />} />
+          <Route path="/empresa" element={<Empresa />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App;
